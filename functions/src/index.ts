5d5a8b453ca89a80d0adb623425201d89b95c9d7
Aaron <aaronc@timpli.com>
2019-10-14 17:49:30 -0700

    added create capabilities

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,6 +16,8 @@ export const firestore = admin.firestore();
  * 4. Remove event
  * 5. Update team member (id)
  * 6. Update event (id)
+ * 7. Delete specific member
+ * 8. Delete specific event
  */ 
 export const getTeam = functions.https.onRequest(async (request, response) => {
     response.set('Access-Control-Allow-Origin', '*');
@@ -24,30 +26,43 @@ export const getTeam = functions.https.onRequest(async (request, response) => {
     const res = await firestore.collection('current')
         .orderBy('order', 'asc').get()
         .then(d => d.docs.map(item => item.data()));
-    return response.json(res);
+    return response.status(200).json(res);
 });
 
 export const getEvents = functions.https.onRequest(async (request, response) => {
     response.set('Access-Control-Allow-Origin', '*');
     response.set('Access-Control-Allow-Methods', 'GET');
 
-    response.set('Access-Control-Allow-Origin', '*');
     const res = await firestore.collection('events').get()
         .then(d => d.docs.map(item => item.data()));
-    return response.json(res);
+    return response.status(200).json(res);
 });
 
 export const addMember = functions.https.onRequest(async (request, response) => {
+    response.set('Access-Control-Allow-Origin', '*');
+    response.set('Access-Control-Allow-Methods', 'POST');
+
     const req = request.body; // JSON object
     
-    return await firestore.collection("current").add(req).then(() => response.status(200).json("Added member!"));
-    //const proc = await firestore.collection("current").add(req).then((r) => console.log(r));
-    //return response.json(req);
+    return await firestore.collection("current").add(req).then(() => response.status(200).json("Added member!"))
+                    .catch(err => response.json("Error: " + err));
 });
 
 export const addEvent = functions.https.onRequest(async (request, response) => {
+    response.set('Access-Control-Allow-Origin', '*');
+    response.set('Access-Control-Allow-Methods', 'POST');
+
     const req = request.body;
 
     return await firestore.collection("events").add(req).then(() => response.status(200).json("Added event!"))
                     .catch(err => response.json("Error: " + err));
 });
+
+export const test = functions.https.onRequest(async (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'POST');
+
+    const r = req.body;
+    return res.json(r);
+});
+
